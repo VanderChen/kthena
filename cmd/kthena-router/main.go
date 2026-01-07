be14173b4cef24d88f48e8e3057a97820d7a4dbc
YaoZengzeng <yaozengzeng@huawei.com>
2026-01-07 14:42:09 +0800

    fix comment
    Signed-off-by: YaoZengzeng <yaozengzeng@huawei.com>

--- cmd/kthena-router/main.go
+++ cmd/kthena-router/main.go
@@ -54,10 +54,10 @@ func main() {
 	)
 
 	klog.InitFlags(nil)
-	// Some dependencies may install a backing logr logger into klog via init().
-	// That can cause V(...) logs to be filtered by that logger's own verbosity
-	// rules (which are not necessarily controlled by klog's -v/--v).
-	// Clear it here so klog verbosity works as expected for this binary.
+	// Some dependencies can override the klog logger via init(), which breaks
+	// verbosity flags. Reset the logger to its default state to fix this.
+	// NOTE: klog.ClearLogger() is meant for tests, but is a viable fix here.
+	// See issue #624.
 	klog.ClearLogger()
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.StringVar(&routerPort, "port", "8080", "Server listen port")
